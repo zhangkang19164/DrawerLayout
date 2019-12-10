5fa1860a413c46e7a92746c22121eef76e0ea0b5
Android-张康 <921183573@qq.com>
2019-12-10 18:00:50 +0800

    Init2

--- app/src/main/java/self/zhangkang/widget/BaseDrawerLayoutActivity.kt
+++ app/src/main/java/self/zhangkang/widget/BaseDrawerLayoutActivity.kt
@@ -1,16 +1,15 @@
 package self.zhangkang.widget
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import android.text.TextUtils
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContextCompat
+import android.widget.LinearLayout
+import androidx.annotation.ColorInt
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import kotlinx.android.synthetic.main.activity_base_drawer_layout.*
 
@@ -23,17 +22,18 @@ import kotlinx.android.synthetic.main.activity_base_drawer_layout.*
  * @author Android-张康
  */
 @SuppressLint("Registered")
-open class BaseDrawerLayoutActivity : BaseActivity() {
+open class BaseDrawerLayoutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         super.setContentView(R.layout.activity_base_drawer_layout)
+        setSupportActionBar(action_bar)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             real_status_bar.setBackgroundColor(window.statusBarColor)
-            real_toolbar.setBackgroundColor(window.statusBarColor)
+        } else {
+            real_status_bar.setBackgroundColor(getStatusBarColor())
         }
         addStatusBarView(real_status_bar)
-        initCommonTitle(real_toolbar)
         open_drawer_layout.setOnClickListener {
             drawer_layout_root.openDrawer(GravityCompat.START)
         }
@@ -42,62 +42,63 @@ open class BaseDrawerLayoutActivity : BaseActivity() {
 
     override fun setContentView(layoutResID: Int) {
         LayoutInflater.from(this).inflate(layoutResID, real_content_view)
-        title = mTitleText
     }
 
     override fun setContentView(view: View?) {
         real_content_view.removeAllViews()
         real_content_view.addView(view)
-        title = mTitleText
     }
 
     override fun setContentView(view: View?, params: ViewGroup.LayoutParams?) {
         real_content_view.removeAllViews()
         real_content_view.addView(view, params)
-        title = mTitleText
+    }
+
+    fun setDrawerContentView(layoutResID: Int) {
+        LayoutInflater.from(this).inflate(layoutResID, drawer_layout)
+    }
+
+    fun setDrawerContentView(view: View?) {
+        drawer_layout.removeAllViews()
+        drawer_layout.addView(view)
+    }
+
+    fun setDrawerContentView(view: View?, params: ViewGroup.LayoutParams?) {
+        drawer_layout.removeAllViews()
+        drawer_layout.addView(view, params)
+    }
+
+
+    @ColorInt
+    open fun getStatusBarColor(): Int {
+        throw RuntimeException("如果SDK_INT小于21，必须重写该方法")
     }
 
 
     /**
-     * 初始化通用标题 只有左边的返回按钮和中间的标题
-     *
-     * @param toolbar 当前Toolbar
+     * 设置StatusBar的高度
      */
-    private fun initCommonTitle(toolbar: Toolbar) {
-        toolbar.setNavigationIcon(R.drawable.ic_left_black)
-        //如果当前的TextView为null，则创建一个
-        if (null == mTitleTextView) {
-            mTitleTextView = createTitleTextView()
-        } else {
-            val parent = mTitleTextView.parent
-            if (null != parent) {
-                (parent as ViewGroup).removeView(mTitleTextView)
-            }
+    private fun addStatusBarView(view: View) {
+        view.layoutParams = view.layoutParams.apply {
+            width = LinearLayout.LayoutParams.MATCH_PARENT
+            height = getStatusBarHeightPx(view.context)
         }
-        val layoutParams = Toolbar.LayoutParams(
-            Toolbar.LayoutParams.WRAP_CONTENT,
-            Toolbar.LayoutParams.WRAP_CONTENT
-        )
-        layoutParams.gravity = Gravity.CENTER
-        mTitleTextView.layoutParams = layoutParams
-        toolbar.addView(mTitleTextView)
-        if (!TextUtils.isEmpty(mTitleText)) {
-            mTitleTextView.text = mTitleText
-        }
-        toolbar.setNavigationOnClickListener { onBackPressed() }
     }
 
-    private fun createTitleTextView(): TextView {
-        val textView = TextView(this)
-        //设置显示为1行
-        textView.setSingleLine()
-        //设置最多显示多少个字
-        textView.setEms(10)
-        //设置省略号在尾部
-        textView.ellipsize = TextUtils.TruncateAt.END
-        textView.gravity = Gravity.CENTER
-        textView.setTextColor(ContextCompat.getColor(this, android.R.color.white))
-        textView.textSize = 18f
-        return textView
+    /**
+     * 获取当前设备状态栏高度
+     *
+     * @param context 外部调用环境
+     * @return 当前设备状态栏高度，单位为：px
+     */
+    private fun getStatusBarHeightPx(context: Context): Int {
+        var statusBarHeight = -1
+        //获取status_bar_height资源的ID
+        val resourceId = context.resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            //根据资源ID获取相应的尺寸值
+            statusBarHeight = context.resources.getDimensionPixelSize(resourceId)
+        }
+        return statusBarHeight
     }
 }
